21a05baa22ce335e1b01bc93bc9f86405d10de48
Thomas Logan <thomas.logan@collins.com>
2019-01-02 17:33:13 -0600

    update

--- ace-tools/com.collins.atc.agree.command/src/com/collins/atc/agree/command/AgreeCommandRuntimeModule.java
+++ ace-tools/com.collins.atc.agree.command/src/com/collins/atc/agree/command/AgreeCommandRuntimeModule.java
@@ -1,11 +1,9 @@
 package com.collins.atc.agree.command;
 
-import org.osate.xtext.aadl2.parsing.AnnexParserAgent;
-
 public class AgreeCommandRuntimeModule extends org.osate.xtext.aadl2.Aadl2RuntimeModule {
 
 	@Override
 	public Class<? extends org.eclipse.xtext.linking.ILinker> bindILinker() {
-		return AnnexParserAgent.class;
+		return AgreeAgent.class;
 	}
 }
