f47bc0e89b04464b981f658edba155c0dd94f61b
jshackleton <44876426+jshackleton@users.noreply.github.com>
2019-05-08 13:11:21 -0500

    Create tb_publisher_impl.h

---
+++ TA5/unit-tests/AADL/testshare/CAmkES/components/publisher_impl/includes/tb_publisher_impl.h
@@ -0,0 +1,8 @@
+#ifndef __tb_AADL_publisher_impl_types__H
+#define __tb_AADL_publisher_impl_types__H
+
+#include "../../../includes/tb_proc_types.h"
+
+void testshare_publisher_component_init(const int64_t *arg);
+
+#endif // __tb_AADL_publisher_impl_types__H
