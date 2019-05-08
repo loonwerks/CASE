62107f50cbb659b1ea8a3f2d840d38c2a7b38487
jshackleton <44876426+jshackleton@users.noreply.github.com>
2019-05-08 12:46:27 -0500

    Create tb_emitter_t_impl.h

---
+++ TA5/unit-tests/AADL/testepmon/CAmkES/components/emitter_t_impl/includes/tb_emitter_t_impl.h
@@ -0,0 +1,10 @@
+#ifndef __tb_AADL_emitter_t_impl_types__H
+#define __tb_AADL_emitter_t_impl_types__H
+
+#include "../../../includes/tb_proc_types.h"
+
+bool tb_enq_enqueue(const int8_t * tb_enq);
+
+void testepmon_emitter_component_init(const int64_t *arg);
+
+#endif // __tb_AADL_emitter_t_impl_types__H
