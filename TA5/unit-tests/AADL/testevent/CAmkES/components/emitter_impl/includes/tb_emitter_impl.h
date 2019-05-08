52f91a4fa455fe87dac059b91987d6f01ff14ae3
jshackleton <44876426+jshackleton@users.noreply.github.com>
2019-05-08 13:03:06 -0500

    Create tb_emitter_impl.h

---
+++ TA5/unit-tests/AADL/testevent/CAmkES/components/emitter_impl/includes/tb_emitter_impl.h
@@ -0,0 +1,10 @@
+#ifndef __tb_AADL_emitter_impl_types__H
+#define __tb_AADL_emitter_impl_types__H
+
+#include "../../../includes/tb_proc_types.h"
+
+bool tb_emitter_impl_write_e(void);
+
+void testevent_emitter_component_init(const int64_t *arg);
+
+#endif // __tb_AADL_emitter_impl_types__H
