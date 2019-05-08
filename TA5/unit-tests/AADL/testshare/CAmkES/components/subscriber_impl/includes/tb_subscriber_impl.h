8fc638c32293ebb7b9349a0a041bd42e924328b1
jshackleton <44876426+jshackleton@users.noreply.github.com>
2019-05-08 13:14:05 -0500

    Create tb_subscriber_impl.h

---
+++ TA5/unit-tests/AADL/testshare/CAmkES/components/subscriber_impl/includes/tb_subscriber_impl.h
@@ -0,0 +1,8 @@
+#ifndef __tb_AADL_subscriber_impl_types__H
+#define __tb_AADL_subscriber_impl_types__H
+
+#include "../../../includes/tb_proc_types.h"
+
+void testshare_subscriber_component_init(const int64_t *arg);
+
+#endif // __tb_AADL_subscriber_impl_types__H
