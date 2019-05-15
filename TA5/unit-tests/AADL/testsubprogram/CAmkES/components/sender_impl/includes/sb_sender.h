b8de3ebfafacd0b479f92ba8fcbbd167998c7711
jshackleton <44876426+jshackleton@users.noreply.github.com>
2019-05-15 13:38:52 -0500

    Create sb_sender.h

---
+++ TA5/unit-tests/AADL/testsubprogram/CAmkES/components/sender_impl/includes/sb_sender.h
@@ -0,0 +1,8 @@
+#ifndef __sb_AADL_sender_impl_types__H
+#define __sb_AADL_sender_impl_types__H
+
+#include "../../../includes/sb_proc_types.h"
+
+void sender_init(const int64_t *arg);
+
+#endif // __sb_AADL_sender_impl_types__H
