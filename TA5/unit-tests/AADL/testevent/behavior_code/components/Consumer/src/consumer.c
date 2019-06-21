3de89e3b988e390719cd9d8aa3e82d7f468c8e8a
John Shackleton <44876426+jshackleton@users.noreply.github.com>
2019-06-21 10:52:16 -0500

    Update consumer.c

--- TA5/unit-tests/AADL/testevent/behavior_code/components/Consumer/src/consumer.c
+++ TA5/unit-tests/AADL/testevent/behavior_code/components/Consumer/src/consumer.c
@@ -8,12 +8,15 @@
 #include <stdio.h>
 #include <sb_consumer_impl.h>
 
+static int8_t _value;
+
 void testevent_consumer_component_init(const int64_t *in_arg) {
   printf("testevent_consumer_component_init called\n");
+  _value = 0;
 }
 
 void testevent_consumer_s_event_handler() {
 
-  printf("[Consumer] Callback fired.\n");
-
+  printf("[Consumer] Callback %d fired.\n", _value);
+  _value = (_value + 1) % 500;
 }
