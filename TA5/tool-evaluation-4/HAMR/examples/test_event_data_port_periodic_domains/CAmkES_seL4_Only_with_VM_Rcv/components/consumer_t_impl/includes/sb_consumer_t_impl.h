caedabe50378ba1a5408a72763b5fad6f49d0e09
John Shackleton <john.shackleton@adventiumlabs.com>
2020-05-20 16:23:36 -0500

    Added example of event-data-port producer/consumer with the producer a native CAmkES component and the consumer a Linux VM.

---
+++ TA5/tool-evaluation-4/HAMR/examples/test_event_data_port_periodic_domains/CAmkES_seL4_Only_with_VM_Rcv/components/consumer_t_impl/includes/sb_consumer_t_impl.h
@@ -0,0 +1,12 @@
+#ifndef __sb_AADL_consumer_t_impl_types__H
+#define __sb_AADL_consumer_t_impl_types__H
+
+#include <sb_types.h>
+
+bool sb_read_port_dequeue(int8_t *);
+
+void test_event_data_port_consumer_time_triggered_handler(const int64_t * in_arg);
+
+void test_event_data_port_consumer_component_init(const int64_t *arg);
+
+#endif // __sb_AADL_consumer_t_impl_types__H
