6da7e92cbd188815e49165a497357ee6b9c3dd31
Jason Belt <belt@ksu.edu>
2020-07-06 15:14:33 -0500

    [Vagrant] Update Sireum

--- TA5/case-env/case-setup.sh
+++ TA5/case-env/case-setup.sh
@@ -10,7 +10,7 @@ set -Eeuxo pipefail
 : "${SEL4_SCRIPTS_V:=592fb0c03c9727dd37e24b2e9b0a17d541c25388}"
 : "${SEL4_V:=28831f579e3560bd3aa18a3898505f091d66b076}"
 : "${CAMKES_V:=e7f5c6da03fc8a71a5a2e503de9f9004acf3ef2a}"
-: "${SIREUM_V:=c218318637aa45ec2e4b9042c0bdc2a2c7dd4fbf}"
+: "${SIREUM_V:=a0ae67f8526abc8412c98e0ab1ab0fdeafebe5b7}"
 : "${FMIDE_V:=nightly}" # use nightly release by default
 
 export SCRIPT_DIR=$(cd -P $(dirname "$0") && pwd -P)
