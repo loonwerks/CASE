4b6106e867b0323c642deb14ec1946ad7cbb7eae
Luke Mondy <luke.mondy@data61.csiro.au>
2020-07-07 15:48:42 +1000

    [Vagrant] Update seL4 dependency scripts. Moves to using Debian Snapshot to fetch apt sources. Updates CakeML version.

--- TA5/case-env/case-setup.sh
+++ TA5/case-env/case-setup.sh
@@ -7,7 +7,7 @@ set -Eeuxo pipefail
 : "${GIT_USER:=Snail}"
 : "${GIT_EMAIL:=<>}"
 
-: "${SEL4_SCRIPTS_V:=592fb0c03c9727dd37e24b2e9b0a17d541c25388}"
+: "${SEL4_SCRIPTS_V:=df998a4beed9d5109d564d8b0552ab118af25365}"
 : "${SEL4_V:=28831f579e3560bd3aa18a3898505f091d66b076}"
 : "${CAMKES_V:=e7f5c6da03fc8a71a5a2e503de9f9004acf3ef2a}"
 : "${SIREUM_V:=a0ae67f8526abc8412c98e0ab1ab0fdeafebe5b7}"
