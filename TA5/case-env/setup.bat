1f85bc648c4037b57347a6f4ed451e34c9ec569f
Robby <robby@ksu.edu>
2020-08-13 14:59:09 -0500

    [Vagrant] Added a workaround for debian bug #966575.

--- TA5/case-env/setup.bat
+++ TA5/case-env/setup.bat
@@ -1,9 +1,11 @@
 set FIRST_RUN=true
 vagrant up --no-provision
+vagrant ssh -c 'sudo apt-mark hold grub*'
 vagrant ssh -c 'sudo apt update'
 vagrant ssh -c 'sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y'
 vagrant ssh -c 'sudo DEBIAN_FRONTEND=noninteractive apt install -y build-essential linux-headers-amd64 linux-image-amd64 python-pip'
 vagrant halt
 set FIRST_RUN=false
 vagrant up || exit /b
+vagrant ssh -c 'sudo apt-mark unhold grub*'
 vagrant reload
