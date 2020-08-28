d32b37091f5eaddd7a912f5c830d2f0564e3972b
Robby <robby@santoslab.org>
2020-08-28 10:00:15 -0500

    [Vagrant] Upgraded Sireum and adapted snapshot.sh

--- TA5/case-env/snapshot.sh
+++ TA5/case-env/snapshot.sh
@@ -2,7 +2,7 @@
 
 set -exuo pipefail
 
-: "${SNAPSHOT_DATE:=20200701}"
+: "${SNAPSHOT_DATE:=20200717T204551Z}"
 
 sudo tee /etc/apt/sources.list << EOF
 deb http://snapshot.debian.org/archive/debian/$SNAPSHOT_DATE buster main
@@ -28,3 +28,17 @@ EOF
 
 echo "force-unsafe-io" | sudo tee /etc/dpkg/dpkg.cfg.d/02apt-speedup > /dev/null
 echo "Acquire::http {No-Cache=True;};" | sudo tee /etc/apt/apt.conf.d/no-cache > /dev/null
+
+sudo apt-get update -q
+
+sudo apt-get install -y --no-install-recommends -t bullseye apt
+
+current_apt_ver=$(apt-cache policy apt | grep "Installed" | xargs | cut -d' ' -f2)
+
+if printf '2.1.9 needed\n%s have\n' "$current_apt_ver" | sort -rV | head -n 1 | grep -q needed; then
+  for pkg in "libapt-pkg6.0_2.1.10_amd64.deb" "apt_2.1.10_amd64.deb" ; do
+    wget "http://snapshot.debian.org/archive/debian/20200811T150316Z/pool/main/a/apt/$pkg"
+    sudo apt install "./$pkg" -t bullseye -y
+    rm "./$pkg"  # clean-up package
+  done
+fi
