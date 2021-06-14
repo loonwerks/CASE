21d02480bed69cf553bd5536c2ebb1ce9eb2f586
Jason Belt <belt@ksu.edu>
2021-06-14 23:17:02 +0000

    add initial version of tutorial

---
+++ TA5/tool-assessment-4/basic/tutorial/hamr-initial/c/etc_seL4/adapters/consumer_t_i_consumer_consumer/consumer_t_i_consumer_consumer_adapter.c
@@ -0,0 +1,25 @@
+#include <consumer_t_i_consumer_consumer_adapter.h>
+
+Unit base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_initialiseArchitecture(STACK_FRAME_ONLY) {
+  DeclNewStackFrame(caller, "consumer_t_i_consumer_consumer_adapter.c", "", "base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_initialiseArchitecture", 0);
+
+  base_consumer_t_i_consumer_consumer_consumer_initialiseArchitecture(SF_LAST);
+}
+
+Unit base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_initialiseEntryPoint(STACK_FRAME_ONLY) {
+  DeclNewStackFrame(caller, "consumer_t_i_consumer_consumer_adapter.c", "", "base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_initialiseEntryPoint", 0);
+
+  base_consumer_t_i_consumer_consumer_consumer_initialiseEntryPoint(SF_LAST);
+}
+
+Unit base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_computeEntryPoint(STACK_FRAME_ONLY) {
+  DeclNewStackFrame(caller, "consumer_t_i_consumer_consumer_adapter.c", "", "base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_computeEntryPoint", 0);
+
+  base_consumer_t_i_consumer_consumer_consumer_computeEntryPoint(SF_LAST);
+}
+
+art_Bridge_EntryPoints base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_entryPoints(STACK_FRAME_ONLY) {
+  DeclNewStackFrame(caller, "consumer_t_i_consumer_consumer_adapter.c", "", "base_test_event_data_port_periodic_domains_consumer_t_i_consumer_consumer_adapter_entryPoints", 0);
+
+  return base_consumer_t_i_consumer_consumer_consumer_entryPoints(SF_LAST);
+}
---
+++ TA5/tool-assessment-4/basic/tutorial/hamr-initial/slang/src/main/nix/base/SharedMemory_Ext.scala
@@ -0,0 +1,16 @@
+package base
+
+import org.sireum._
+import art._
+
+// This file was auto-generated.  Do not edit
+
+object SharedMemory_Ext {
+  def create(id: Z): Z = halt("stub")
+  def get(id: Z): Z = halt("stub")
+  def send(id: Z, port: Art.PortId, d: DataContent): Unit = halt("stub")
+  def receive(port: Art.PortId, out: MBox2[Art.PortId, DataContent]): Unit = halt("stub")
+  def sendAsync(id: Z, port: Art.PortId, d: DataContent): B = halt("stub")
+  def receiveAsync(port: Art.PortId, out: MBox2[Art.PortId, Option[DataContent]]): Unit = halt("stub")
+  def remove(id: Z): Unit = halt("stub")
+}
