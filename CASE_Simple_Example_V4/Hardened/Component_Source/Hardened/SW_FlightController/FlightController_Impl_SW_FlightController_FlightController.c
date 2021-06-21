fb6f37fb79fa38c3990ff064f661a12b177b3ad2
Karl Hoech <karl.hoech@collins.com>
2021-06-21 14:02:57 -0500

    Update naming to match that generated by HAMR

--- CASE_Simple_Example_V4/Hardened/Component_Source/Hardened/SW_FlightController/FlightController_Impl_SW_FlightController_FlightController.c
+++ CASE_Simple_Example_V4/Hardened/Component_Source/Hardened/SW_FlightController/FlightController_Impl_SW_FlightController_FlightController.c
@@ -6,15 +6,6 @@
 
 Unit HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController_initialise_(STACK_FRAME_ONLY) {
   DeclNewStackFrame(caller, "FlightController_Impl_SW_FlightController_FlightController.c", "", "HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController_initialise_", 0);
-
-  // examples of api setter and logging usage
-/*
-  api_logInfo__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF string("Example logInfo"));
-
-  api_logDebug__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF string("Example logDebug"));
-
-  api_logError__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF string("Example logError"));
-*/
 }
 
 Unit HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController_finalise_(STACK_FRAME_ONLY) {
@@ -26,20 +17,14 @@ Unit HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightControlle
 
   // examples of api getter usage
 
-  uint8_t t0[numBytes_HAMR_Simple_V4_SW_Mission];
+  uint8_t t0[numBytes_HAMR_Simple_V4_Data_Types_Mission];
   size_t t0_numBits;
   if(api_get_FlightPlan__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF &t0_numBits, t0)) {
     // sanity check
-    sfAssert(SF (Z) t0_numBits == numBits_HAMR_Simple_V4_SW_Mission, "numBits received does not match expected");
+    sfAssert(SF (Z) t0_numBits == numBits_HAMR_Simple_V4_Data_Types_Mission, "numBits received does not match expected");
     printf("FLIGHTCONTROLLER RECEIVE\n");
     print_Mission((const Mission *)t0);
     printf("\n");
-/*
-    DeclNewString(FlightPlan_str);
-    String__append(SF (String) &FlightPlan_str, string("Received on FlightPlan: "));
-    byte_array_string(SF (String) &FlightPlan_str, t0, numBytes_HAMR_Simple_V4_SW_Mission);
-    api_logInfo__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF (String) &FlightPlan_str);
-*/
   }
 
   if(api_get_Alert__HAMR_Simple_V4_SW_FlightController_Impl_SW_FlightController_FlightController(SF_LAST )){
