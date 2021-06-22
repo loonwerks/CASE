edd2d2853967b1c25d7520cc91ee588443d2320a
Eric Mercer <egm@cs.byu.edu>
2021-06-22 15:14:38 -0600

    Fix typo in source for flightplanner

--- CASE_Simple_Example_V4/Hardened/Component_Source/Hardened/SW_FlightPlanner/FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner.c
+++ CASE_Simple_Example_V4/Hardened/Component_Source/Hardened/SW_FlightPlanner/FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner.c
@@ -15,11 +15,11 @@ Unit HAMR_Simple_V4_SW_FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner_finalis
 Unit HAMR_Simple_V4_SW_FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner_timeTriggered_(STACK_FRAME_ONLY) {
   DeclNewStackFrame(caller, "FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner.c", "", "HAMR_Simple_V4_SW_FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner_timeTriggered_", 0);
 
-  uint8_t t0[numBytes_HAMR_Simple_V4_Data_Types_Mission];
+  uint8_t t0[numBytes_HAMR_Simple_V4_Data_Types_RF_Msg_Impl];
   size_t t0_numBits;
   if(api_get_MissionCommand__HAMR_Simple_V4_SW_FlightPlanner_Impl_SW_FlightPlanner_FlightPlanner(SF &t0_numBits, t0)) {
     // sanity check
-    sfAssert(SF (Z) t0_numBits == numBits_HAMR_Simple_V4_Data_Types_Mission, "numBits received does not match expected");
+    sfAssert(SF (Z) t0_numBits == numBytes_HAMR_Simple_V4_Data_Types_RF_Msg_Impl, "numBits received does not match expected");
     uint8_t* output = getFlightPlannerOutput(t0);
     if (output != NULL) {
       printf("FLIGHTPLANNER SEND\n");
