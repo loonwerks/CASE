cd4cff718b70995b201b4e310299568ea81e2184
iamundson <36237616+iamundson@users.noreply.github.com>
2021-12-15 10:34:11 -0600

    Update make_fmide.sh

--- TA5/make_fmide.sh
+++ TA5/make_fmide.sh
@@ -4,7 +4,7 @@ set -Eeuxo pipefail
 
 : "${FMIDE_DIR:=fmide}"
 : "${eclipseRelease:=2020-06}"
-: "${OSATE_VERSION:=2.9.0}"
+: "${OSATE_VERSION:=2.9.2}"
 : "${OSATE_URL:=https://osate-build.sei.cmu.edu/download/osate/stable/${OSATE_VERSION}/products/osate2-${OSATE_VERSION}-vfinal-linux.gtk.x86_64.tar.gz}"
 : "${FMW_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/formal-methods-workbench-updates/master/}"
 : "${AGREE_UPDATE_SITE:=http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/agree/}"
@@ -13,8 +13,6 @@ set -Eeuxo pipefail
 : "${RESOLUTE_FEATURE_ID:=com.rockwellcollins.atc.resolute.feature.feature.group}"
 : "${BRIEFCASE_UPDATE_SITE:=https://download.eclipse.org/releases/$eclipseRelease,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/briefcase/}"
 : "${BRIEFCASE_FEATURE_ID:=com.collins.trustedsystems.briefcase.feature.feature.group}"
-: "${AMASE_UPDATE_SITE:=http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/amase/}"
-: "${AMASE_FEATURE_ID:=edu.umn.cs.crisys.safety.feature.feature.group}"
 : "${HAMR_UPDATE_SITE:=https://raw.githubusercontent.com/sireum/hamr-plugin-update-site/master}"
 : "${HAMR_FEATURE_ID:=org.sireum.aadl.osate.hamr.feature.feature.group}"
 : "${METALS_UPDATE_SITE:=https://download.eclipse.org/releases/$eclipseRelease,http://scalameta.org/metals-eclipse/update}"
@@ -29,7 +27,5 @@ popd
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${AGREE_UPDATE_SITE} -installIU ${AGREE_FEATURE_ID}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${RESOLUTE_UPDATE_SITE} -installIU ${RESOLUTE_FEATURE_ID}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${BRIEFCASE_UPDATE_SITE} -installIU ${BRIEFCASE_FEATURE_ID}
-${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${AMASE_UPDATE_SITE} -installIU ${AMASE_FEATURE_ID}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${HAMR_UPDATE_SITE} -installIU ${HAMR_FEATURE_ID}
 ${FMIDE_DIR}/osate -nosplash -console -consoleLog -application org.eclipse.equinox.p2.director -repository ${METALS_UPDATE_SITE} -installIU ${METALS_FEATURE_ID}
-
