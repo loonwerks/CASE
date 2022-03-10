c75a6c0fc1f05d310225b26a13600465af864d99
iamundson <36237616+iamundson@users.noreply.github.com>
2022-03-10 09:42:30 -0600

    Update to OSATE 2.10.2

--- TA5/case-env/bin/fmide.sh
+++ TA5/case-env/bin/fmide.sh
@@ -4,16 +4,16 @@ set -Eeuxo pipefail
 
 : "${BASE_DIR:=$HOME/CASE}"
 : "${FMIDE_DIR:=$BASE_DIR/FMIDE}"
-: "${eclipseRelease:=2020-06}"
-: "${OSATE_VERSION:=2.9.0}"
+: "${eclipseRelease:=2021-03}"
+: "${OSATE_VERSION:=2.10.2}"
 : "${OSATE_URL:=https://osate-build.sei.cmu.edu/download/osate/stable/${OSATE_VERSION}/products/osate2-${OSATE_VERSION}-vfinal-linux.gtk.x86_64.tar.gz}"
-: "${AGREE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/AGREE-Updates/master/agree_2.7.0/}"
+: "${AGREE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/AGREE-Updates/master/,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/agree/}"
 : "${AGREE_FEATURE_ID:=com.rockwellcollins.atc.agree.feature.feature.group}"
-: "${RESOLUTE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/Resolute-Updates/master/resolute_2.7.1/}"
+: "${RESOLUTE_UPDATE_SITE:=https://raw.githubusercontent.com/loonwerks/Resolute-Updates/master/,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/resolute/}"
 : "${RESOLUTE_FEATURE_ID:=com.rockwellcollins.atc.resolute.feature.feature.group}"
-: "${BRIEFCASE_UPDATE_SITE:=https://download.eclipse.org/releases/$eclipseRelease,https://raw.githubusercontent.com/loonwerks/BriefCASE-Updates/master/briefcase_0.5.2/}"
+: "${BRIEFCASE_UPDATE_SITE:=https://download.eclipse.org/releases/$eclipseRelease,https://raw.githubusercontent.com/loonwerks/BriefCASE-Updates/master/,http://ca-trustedsystems-dev-us-east-1.s3-website-us-east-1.amazonaws.com/p2/snapshots/briefcase/}"
 : "${BRIEFCASE_FEATURE_ID:=com.collins.trustedsystems.briefcase.feature.feature.group}"
-: "${HAMR_UPDATE_SITE:=https://raw.githubusercontent.com/sireum/hamr-plugin-update-site/CASE-Tool-Assessment-4}"
+: "${HAMR_UPDATE_SITE:=https://raw.githubusercontent.com/sireum/hamr-plugin-update-site/master}"
 : "${HAMR_FEATURE_ID:=org.sireum.aadl.osate.hamr.feature.feature.group}"
 
 rm -rf ${FMIDE_DIR}
